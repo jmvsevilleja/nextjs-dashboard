e661e3f9feeb8efe05b449b6e20445266e59ef9e
Jess Sevilleja <jmvsevilleja@gmail.com>
2025-04-19 08:02:00 +0800

    added delay

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -17,12 +17,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log("Fetching revenue data...");
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log("Data fetch completed after 3 seconds.");
 
     return data;
   } catch (error) {
@@ -34,12 +34,17 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   // noStore(); // Prevent caching of this data fetch
   try {
+    console.log("Fetching invoice data...");
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
     const data = await sql<LatestInvoiceRaw[]>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
+
+    console.log("Data fetch completed after 3 seconds.");
 
     const latestInvoices = data.map((invoice) => ({
       ...invoice,
